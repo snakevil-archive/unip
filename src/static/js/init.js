3df5ab80684e8a98a4ba6cca8d9645070358ad78
Snakevil Zen <zsnakevil@gmail.com>
2012-03-30 01:06:01 +0800

    Services list in page re-grouped and re-sorted for the previous damn OVERWRITE merge.

--- src/static/js/init.js
+++ src/static/js/init.js
@@ -22,81 +22,103 @@
  */
 
 $(function(){
-	function isSingleTouch(event){
-		return event.touches.length === 1 && event.targetTouches.length === 1 && event.changedTouches.length === 1;
-	}
-	function initViewport(handler){
-		var target = handler.querySelector(".vp_b");
-		if(target){
-			function start(e){
-				if(isSingleTouch(e)){
-					$(handler)
-						.on("touchmove", move, false)
-						.on("touchend", end, false)
-						.on("touchcancel", end, false);
-				}
-			}
-			function move(e){
-				if(isSingleTouch(e)){
+    function isSingleTouch(event){
+        return event.touches.length === 1 && event.targetTouches.length === 1 && event.changedTouches.length === 1;
+    }
+    function initViewport(handler){
+        var target = handler.querySelector(".vp_b");
+        if(target){
+            function start(e){
+                if(isSingleTouch(e)){
+                    $(handler)
+                        .on("touchmove", move, false)
+                        .on("touchend", end, false)
+                        .on("touchcancel", end, false);
+                }
+            }
+            function move(e){
+                if(isSingleTouch(e)){
 
-				}
-			}
-			function end(e){
-				if(isSingleTouch(e)){
-					$$$(handler)
-						.un("touchmove", move, false)
-						.un("touchend", end, false)
-						.un("touchcancel", end, false);
-				}
-			}
-			bind(handler, "touchstart", start, false);
-		}
-	}
-	if(document.querySelector(".jt_vp")){
-		initViewport(document.querySelector(".jt_vp"));
-	}
-	$("#Account")
-		.val(localStorage[$("#Service").val()] || "")
-		.on("keyup", function(e){
-			//localStorage["user_account"] = this.value.trim();
-			localStorage[$("#Service").val()] = this.value.trim();
-			UNIP.Calc();
-		});
+                }
+            }
+            function end(e){
+                if(isSingleTouch(e)){
+                    $(handler)
+                        .un("touchmove", move, false)
+                        .un("touchend", end, false)
+                        .un("touchcancel", end, false);
+                }
+            }
+            bind(handler, "touchstart", start, false);
+        }
+    }
+    if(document.querySelector(".jt_vp")){
+        initViewport(document.querySelector(".jt_vp"));
+    }
+    $("#Account")
+        .val(localStorage[$("#Service").val()] || "")
+        .on("keyup", function(e){
+            //localStorage["user_account"] = this.value.trim();
+            localStorage[$("#Service").val()] = this.value.trim();
+            UNIP.Calc();
+        });
 
-	$("#Password").on("keydown", function(e){
-		if(!((e.keyCode === 67 || e.keyCode === 65) && (e.metaKey || e.ctrlKey))){
-			e.preventDefault();
-		}
-	});
-	$("#Seed").on("keyup", UNIP.Calc);
-	$(document).bind("touchstart", function(e){
-		if(!(/^(?:input|select|textarea)$/i.test(e.target.nodeName))){
-			e.preventDefault();
-		}
-	}, true);
-	var services = [], html = '';
-	for (var service in UNIP.rules)
-		services[services.length] = service;
-	for (var i = 0, title, domains; i < services.length; i++) {
-		title = UNIP.rules[services[i]].title,
-		domains = services[i].split('.');
-		if (!title)
-			domains[0] = domains[0].charAt(0).toUpperCase() + domains[0].substr(1),
-			title = domains.join('.');
-		else if (title.toLowerCase() == domains[0].toLowerCase())
-			domains[0] = title,
-			title = domains.join('.');
-		else
-			title += ' (' + services[i] + ')';
-		html += '<option value="' + services[i] + '">' + title + '</option>';
-	}
-	$('#Service')
-		.html(html)
-		.on("change", function(e){
-			$("#Account").val(localStorage[this.value] || "");
-			UNIP.Calc();
-		});
-	UNIP.Calc()
+    $("#Password").on("keydown", function(e){
+        if(!((e.keyCode === 67 || e.keyCode === 65) && (e.metaKey || e.ctrlKey))){
+            e.preventDefault();
+        }
+    });
+    $("#Seed").on("keyup", UNIP.Calc);
+    $(document).bind("touchstart", function(e){
+        if(!(/^(?:input|select|textarea)$/i.test(e.target.nodeName))){
+            e.preventDefault();
+        }
+    }, true);
+    var services = {},
+    html = '',
+    type = '';
+    for (var service in UNIP.rules) {
+        service = service.toLowerCase();
+        type = service.charAt(0);
+        if (96 < type.charCodeAt(0) && type.charCodeAt(0) < 123)
+            type = type.toUpperCase();
+        else
+            type = '#';
+        if (!services[type])
+            services[type] = [];
+        if (-1 == services[type].indexOf(service))
+            services[type][services[type].length] = service;
+    }
+    for (var i = 64, j, k, title, domains; i < 91; i++) {
+        j = String.fromCharCode(i);
+        if ('@' == j)
+            j = '#';
+        if (!services[j])
+            continue;
+        html += '<optgroup label="' + j + '">';
+        services[j] = services[j].sort();
+        for (k = 0; k < services[j].length; k++) {
+            title = UNIP.rules[services[j][k]].title,
+            domains = services[j][k].split('.');
+            if (!title)
+                domains[0] = domains[0].charAt(0).toUpperCase() + domains[0].substr(1),
+                title = domains.join('.');
+            else if (title.toLowerCase() == domains[0].toLowerCase())
+                domains[0] = title,
+                title = domains.join('.');
+            else
+                title += ' (' + services[j][k] + ')';
+            html += '<option value="' + services[j][k] + '">' + title + '</option>';
+        }
+        html += '</optgroup>';
+    }
+    $('#Service')
+        .html(html)
+        .on("change", function(e){
+            $("#Account").val(localStorage[this.value] || "");
+            UNIP.Calc();
+        });
+    UNIP.Calc()
 });
 
 // vim: se ft=javascript fenc=utf-8 ff=unix:
